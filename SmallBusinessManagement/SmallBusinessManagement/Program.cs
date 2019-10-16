e295cc5c3deac63095edccca7ccb76101f408275
Ariful Islam <arifulcse2743@gmail.com>
2019-10-17 02:04:30 +0600

    Prome

--- SmallBusinessManagement/SmallBusinessManagement/Program.cs
+++ SmallBusinessManagement/SmallBusinessManagement/Program.cs
@@ -16,7 +16,6 @@ namespace SmallBusinessManagement
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new CategoryUi());
             Application.Run(new ProductUi());
         }
     }
